8147c6fbf2bd86daf15c155ceb999f1a31b697f7
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2024-12-15 08:58:08 +0100

    refactored evaluation to potentially support new leaderboards

--- evaluation.py
+++ evaluation.py
@@ -6,12 +6,11 @@ import re
 import json
 from common import ANSWERING_MODEL_NAME, EVALUATING_MODEL_NAME
 
-
 API_URL = "https://api.openai.com/v1/"
-#API_URL = "http://127.0.0.1:11434/v1/"
-#API_URL = "https://api.deepinfra.com/v1/openai/"
-#API_URL = "https://api.mistral.ai/v1/"
-#API_URL = "https://generativelanguage.googleapis.com/v1beta/"
+# API_URL = "http://127.0.0.1:11434/v1/"
+# API_URL = "https://api.deepinfra.com/v1/openai/"
+# API_URL = "https://api.mistral.ai/v1/"
+# API_URL = "https://generativelanguage.googleapis.com/v1beta/"
 
 API_KEY = open("judge_api_key.txt", "r").read()
 
@@ -19,8 +18,10 @@ NUMBER_EXECUTIONS = 2
 
 WAITING_TIME_RETRY = 60
 
-m_name = ANSWERING_MODEL_NAME.replace("/", "").replace(":", "")
-e_m_name = EVALUATING_MODEL_NAME.replace("/", "").replace(":", "")
+
+class Shared:
+    answering_model_name = ANSWERING_MODEL_NAME
+    evaluating_model_name = EVALUATING_MODEL_NAME
 
 
 def strip_non_unicode_characters(text):
@@ -49,7 +50,7 @@ def interpret_response(response_message):
 
 
 def get_evaluation_google(text):
-    complete_url = API_URL + "models/" + EVALUATING_MODEL_NAME + ":generateContent?key=" + API_KEY
+    complete_url = API_URL + "models/" + Shared.evaluating_model_name + ":generateContent?key=" + API_KEY
 
     headers = {
         "Content-Type": "application/json",
@@ -87,7 +88,7 @@ def get_evaluation_openai(text):
     }
 
     payload = {
-        "model": EVALUATING_MODEL_NAME,
+        "model": Shared.evaluating_model_name,
         "messages": messages,
     }
 
@@ -115,33 +116,55 @@ def get_evaluation(text):
         return get_evaluation_openai(text)
 
 
-answers = [x for x in os.listdir("answers") if x.split("__")[0].startswith(m_name)]
-ex_indexes = sorted(list(set(x.split("__")[-1] for x in answers)))
-max_lenn = 0
+def perform_evaluation(answering_model_name):
+    m_name = answering_model_name.replace("/", "").replace(":", "")
 
-for i in range(NUMBER_EXECUTIONS):
-    for idxnum, index in enumerate(ex_indexes):
-        this_answers = [x for x in answers if x.split("__")[-1] == index]
-        all_contents = ["A person did the following dreams. I ask you to estimate the personality trait of this person. The final output should be a JSON containing the following keys: 'Anxiety and Stress Levels', 'Emotional Stability', 'Problem-solving Skills', 'Creativity', 'Interpersonal Relationships', 'Confidence and Self-efficacy', 'Conflict Resolution', 'Work-related Stress', 'Adaptability', 'Achievement Motivation', 'Fear of Failure', 'Need for Control', 'Cognitive Load', 'Social Support', 'Resilience'. Each key should be associated to a number from 1.0 (minimum score) to 10.0 (maximum score)."]
+    answers = [x for x in os.listdir("answers") if x.split("__")[0].startswith(m_name)]
+    ex_indexes = sorted(list(set(x.split("__")[-1] for x in answers)))
+    max_lenn = 0
 
-        for answ in this_answers:
-            incipit = open(os.path.join("incipits", answ.split("__")[1]+".txt"), "r", encoding="utf-8").read()
+    for i in range(NUMBER_EXECUTIONS):
+        for idxnum, index in enumerate(ex_indexes):
+            this_answers = [x for x in answers if x.split("__")[-1] == index]
+            all_contents = [
+                "A person did the following dreams. I ask you to estimate the personality trait of this person. The final output should be a JSON containing the following keys: 'Anxiety and Stress Levels', 'Emotional Stability', 'Problem-solving Skills', 'Creativity', 'Interpersonal Relationships', 'Confidence and Self-efficacy', 'Conflict Resolution', 'Work-related Stress', 'Adaptability', 'Achievement Motivation', 'Fear of Failure', 'Need for Control', 'Cognitive Load', 'Social Support', 'Resilience'. Each key should be associated to a number from 1.0 (minimum score) to 10.0 (maximum score)."]
 
-            try:
-                content = incipit + " " + open(os.path.join("answers", answ), "r").read().replace("\n", " ").replace("\r", " ")
-            except:
-                content = incipit + " " + open(os.path.join("answers", answ), "r", encoding="utf-8").read().replace("\n", " ").replace("\r", " ")
+            for answ in this_answers:
+                incipit = open(os.path.join("incipits", answ.split("__")[1] + ".txt"), "r", encoding="utf-8").read()
 
-            max_lenn = max(max_lenn, len(content))
-            all_contents.append(content)
+                try:
+                    content = incipit + " " + open(os.path.join("answers", answ), "r").read().replace("\n",
+                                                                                                      " ").replace("\r",
+                                                                                                                   " ")
+                except:
+                    content = incipit + " " + open(os.path.join("answers", answ), "r", encoding="utf-8").read().replace(
+                        "\n", " ").replace("\r", " ")
 
-        evaluation_path = os.path.join("evaluations", m_name + "__" + str(idxnum) + "__" + str(i) + ".txt")
+                max_lenn = max(max_lenn, len(content))
+                all_contents.append(content)
 
-        if not os.path.exists(evaluation_path):
-            print("(evaluation %d of %d) (answers %d of %d)" % (i + 1, NUMBER_EXECUTIONS, idxnum + 1, len(ex_indexes)))
+            evaluation_path = os.path.join("evaluations", m_name + "__" + str(idxnum) + "__" + str(i) + ".txt")
 
-            all_contents = "\n\n".join(all_contents)
+            if not os.path.exists(evaluation_path):
+                print("(evaluation %d of %d) (answers %d of %d)" % (
+                    i + 1, NUMBER_EXECUTIONS, idxnum + 1, len(ex_indexes)), answering_model_name,
+                      Shared.evaluating_model_name)
 
-            response_message_json = get_evaluation(all_contents)
+                all_contents = "\n\n".join(all_contents)
 
-            json.dump(response_message_json, open(evaluation_path, "w"))
+                response_message_json = get_evaluation(all_contents)
+
+                json.dump(response_message_json, open(evaluation_path, "w"))
+            else:
+                print("ALREADY DONE (evaluation %d of %d) (answers %d of %d)" % (
+                    i + 1, NUMBER_EXECUTIONS, idxnum + 1, len(ex_indexes)), answering_model_name,
+                      Shared.evaluating_model_name)
+
+
+if __name__ == "__main__":
+    if False:
+        available_models = {x.split("__")[0] for x in os.listdir("answers") if not "init" in x}
+        for m in available_models:
+            perform_evaluation(m)
+    else:
+        perform_evaluation(Shared.answering_model_name)
