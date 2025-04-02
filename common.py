b0bcb17f8b9ce885c9a9ceb309563768a410ef13
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-04-02 08:38:29 +0200

    refactoring

--- common.py
+++ common.py
@@ -1,4 +1,6 @@
-ANSWERING_MODEL_NAME = "gpt-4o"
-EVALUATING_MODEL_NAME = "gpt-4o-mini"
-EVALUATION_FOLDER = "evaluations"
+
+
+ANSWERING_MODEL_NAME = "qwen2.5-omni-7b"
+EVALUATING_MODEL_NAME = "gpt-4.5-preview"
+EVALUATION_FOLDER = "evaluations-gpt45"
 TARGET_GIT_TABLE_RESULT = "results_gpt_45.md"
