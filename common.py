41a2104761ab4946d90d8f7330660657135ab442
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-04-02 08:50:35 +0200

    adding qwen-2.5-32b

--- common.py
+++ common.py
@@ -1,20 +1,27 @@
 ANSWERING_MODEL_NAME = "qwen2.5-omni-7b"
 EVALUATING_MODEL_NAME = "gpt-4.5-preview"
+EVALUATING_MODEL_NAME = "qwen-2.5-32b"
 
 
 def get_evaluation_folder():
     if "gpt-4.5" in EVALUATING_MODEL_NAME:
         return "evaluations-gpt45"
+    elif "qwen-2.5-32b" in EVALUATING_MODEL_NAME:
+        return "evaluations-qwen25-32b"
 
 
 def get_git_table_result():
     if "gpt-4.5" in EVALUATING_MODEL_NAME:
         return "results_gpt_45.md"
+    elif "qwen-2.5-32b" in EVALUATING_MODEL_NAME:
+        return "alt_results_qwen25-32b.md"
 
 
 def get_evaluation_api_url():
     if "gpt-4.5" in EVALUATING_MODEL_NAME:
         return "https://api.openai.com/v1/"
+    elif "qwen-2.5-32b" in EVALUATING_MODEL_NAME:
+        return "https://api.groq.com/openai/v1/"
 
 
 EVALUATION_FOLDER = get_evaluation_folder()
