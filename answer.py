ae56c2f05f0bb00baf1c13660f1ed5f35362c08f
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-04-05 05:41:02 +0200

    added Openrouter URL

--- answer.py
+++ answer.py
@@ -19,6 +19,7 @@ API_URL = "https://api.openai.com/v1/"
 #API_URL = "https://dashscope-intl.aliyuncs.com/compatible-mode/v1/"
 #API_URL = "https://api.anthropic.com/v1/"
 #API_URL = "https://integrate.api.nvidia.com/v1/"
+#API_URL = "https://openrouter.ai/api/v1/"
 
 API_KEY = open("api_key.txt", "r").read()
 
